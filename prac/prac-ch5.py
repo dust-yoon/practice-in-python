a2f0ee5ce7c49fe12e34c339e51ed311774fb9d1
JIYUN <popowjy12@konkuk.ac.kr>
2024-05-30 23:55:38 +0900

    feat: project chapter 5

---
+++ prac/prac-ch5.py
@@ -0,0 +1,28 @@
+# 리스트 []
+# 지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+
+subway = [10, 20, 30]
+print(subway)
+
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+#조세호씨가 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호"))
+#하하 씨가 다음 정류장에서 다음 칸에 탐
+subway.append("하하")
+print(subway)
+# 정형돈 씨를 유재석/조세호 사이에 태워봄
+subway.insert(1, "정형돈")
+print(subway)
+# 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway)
+# 같은 이름의 사람이 몇 명 있는지 확인
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+#정렬도 가능
