9f7e4f7d87c92c1354a6e6b6d3d5b5e5ec7562e2
JIYUN <popowjy12@konkuk.ac.kr>
2024-08-06 23:25:13 +0900

    백준 1000

---
+++ BAEKJOON/1000~2_240729.py
@@ -0,0 +1,12 @@
+# 1000
+numbers = input()
+print( int(numbers[:-2]) + int(numbers[-1]))
+# 1001
+numbers = input()
+print( int(numbers[:-2]) - int(numbers[-1]))
+# 1002
+numbers = input()
+print( int(numbers[:-2]) * int(numbers[-1]))
+# 1008
+numbers = input()
+print( int(numbers[:-2]) / int(numbers[-1]))
