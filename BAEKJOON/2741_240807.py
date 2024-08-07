d01ff385c978eb33017f514b88953663937c61d3
JIYUN <popowjy12@konkuk.ac.kr>
2024-08-07 21:53:13 +0900

    백준 2741

---
+++ BAEKJOON/2741_240807.py
@@ -0,0 +1,31 @@
+# # 2741
+# number = int(input())
+# for i in range(1,number+1):
+#     print(i)
+
+# # 10872
+# number = int(input())
+# answer = 1
+# for i in range(1, number+1):
+#     answer *= i
+# print(answer)
+
+# # 10950
+# number = int(input())
+# for i in range(number):
+#     test = input()
+#     print(int(test[0]) + int(test[-1]))
+
+# 10952
+number = input()
+while number == '0 0':
+    print(int(number[0])+int(number[-1]))
+
+
+
+# 1 1
+# 2 3
+# 3 4
+# 9 8
+# 5 2
+# 0 0
